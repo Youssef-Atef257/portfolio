270bfb9f2117e82010eea066aa261535eecf64a1
Youssef Attia <youssefatef257@gmail.com>
2025-03-15 18:26:32 -0700

    used tilt from react-parallax-tilt to try to solve failure of deployment

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Tilt from "react-tilt";
+// import Tilt from "react-tilt";
+import Tilt from 'react-parallax-tilt';
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -18,7 +19,7 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
+      {/* <Tilt
         options={{
           max: 45,
           scale: 1,
@@ -62,7 +63,58 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
-      </Tilt>
+      </Tilt> */}
+
+
+<Tilt
+  tiltMaxAngleX={45}  // Replaces "max: 45"
+  tiltMaxAngleY={45}
+  scale={1.1}         // Similar to "scale: 1"
+  transitionSpeed={450} // Replaces "speed: 450"
+  className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+>
+  <div className='relative w-full h-[230px]'>
+    <img
+      src={image}
+      alt='project_image'
+      className='w-full h-full object-cover rounded-2xl'
+    />
+
+    <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+      <div
+        onClick={() => window.open(source_code_link, "_blank")}
+        className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+      >
+        <img
+          src={github}
+          alt='source code'
+          className='w-1/2 h-1/2 object-contain'
+        />
+      </div>
+    </div>
+  </div>
+
+  <div className='mt-5'>
+    <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+    <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+  </div>
+
+  <div className='mt-4 flex flex-wrap gap-2'>
+    {tags.map((tag) => (
+      <p
+        key={`${name}-${tag.name}`}
+        className={`text-[14px] ${tag.color}`}
+      >
+        #{tag.name}
+      </p>
+    ))}
+  </div>
+</Tilt>
+
+
+
+
+
     </motion.div>
   );
 };
